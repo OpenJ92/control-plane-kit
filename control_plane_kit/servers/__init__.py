174066793bcb74b4b31ea79a66da94dbd79bec69
Jacob Vartuli-Schonberg <jacob.vartuli.92@gmail.com>
2026-07-14 13:25:39 -0400

    Add hello server block factory (#64)

--- control_plane_kit/servers/__init__.py
+++ control_plane_kit/servers/__init__.py
@@ -1,5 +1,12 @@
 """Optional server adapters for control-plane-kit."""
 
 from control_plane_kit.servers.block_control import BlockControlState, create_block_control_app
+from control_plane_kit.servers.hello import HelloEnvironment, hello_command, hello_server_block
 
-__all__ = ["BlockControlState", "create_block_control_app"]
+__all__ = [
+    "BlockControlState",
+    "HelloEnvironment",
+    "create_block_control_app",
+    "hello_command",
+    "hello_server_block",
+]
