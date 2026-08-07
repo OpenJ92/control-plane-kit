b902d2bd71ad2566c64a67e4989af477423d1200
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-08-07 15:15:17 -0400

    test: extend migration rebuild identity set

--- control-plane-kit-operations/tests/test_schema_migration_runner.py
+++ control-plane-kit-operations/tests/test_schema_migration_runner.py
@@ -123,6 +123,8 @@ class PostgresSchemaMigrationRunnerTests(unittest.TestCase):
                 "cpk_activity_runs_started_check",
                 "cpk_execution_requests_claim_check",
                 "cpk_operation_sessions_closed_check",
+                "cpk_secret_providers_revocation_evidence_check",
+                "cpk_secret_references_revocation_evidence_check",
             }
             self.assertLessEqual(rebuilt, set(before))
             for constraint, (identity, definition) in before.items():
@@ -133,7 +135,11 @@ class PostgresSchemaMigrationRunnerTests(unittest.TestCase):
                         self.assertNotEqual(after_identity, identity)
                     else:
                         self.assertEqual(after_identity, identity)
-            rebuilt_indexes = {"cpk_observations_latest_subject"}
+            rebuilt_indexes = {
+                "cpk_observations_latest_subject",
+                "cpk_secret_providers_history",
+                "cpk_secret_references_history",
+            }
             self.assertLessEqual(rebuilt_indexes, set(before_indexes))
             for index, (identity, definition) in before_indexes.items():
                 with self.subTest(index=index):
